77672e0113f5d02e31ccbe371137bddf398e8b6f
Gensthaler Octavian <octav.gensthaler@gmail.com>
2014-07-15 12:59:33 -0700

    Big LUA update
    - Added scripts folder
    - Added items.lua where the item definitions are
    - Added LuaScript binder
    - Updated icon items to use an iconID

---
+++ Scripts/items.lua
@@ -0,0 +1,89 @@
+----------------------------
+-- POTION OF HEALTH
+----------------------------
+item_0 = {
+	name = "Potion of Healing",
+	description = "The potion is releasing\na bright red glow!",
+	max_number = 5,
+	weight = 0.5,
+	value = 25,
+	category = "FOOD",
+	equipable = false,
+	craftable = true,
+	iconID = 0
+}
+
+----------------------------
+-- HEALTH FLASK
+----------------------------
+item_1 = {
+	name = "Health Flask",
+	description = "This container holds\nmore than a bottle.",
+	max_number = 1,
+	weight = 1,
+	value = 50,
+	category = "FOOD",
+	equipable = false,
+	craftable = true,
+	iconID = 1
+}
+
+----------------------------
+-- RUSTY BLADE
+----------------------------
+item_2 = {
+	name = "Rusty Blade",
+	description = "Slicing, but not so\nmuch dicing...",
+	max_number = 1,
+	weight = 4,
+	value = 10,
+	category = "EQUIPMENT",
+	equipable = true,
+	craftable = true,
+	iconID = 7
+}
+
+----------------------------
+-- DUSTY TOME
+----------------------------
+item_3 = {
+	name = "Dusty Tome",
+	description = "The letters on the\ntitle are all scratched.\nI can't read them",
+	max_number = 1,
+	weight = 1,
+	value = 15,
+	category = "BOOKS",
+	equipable = false,
+	craftable = false,
+	iconID = 2
+}
+
+----------------------------
+-- SCROLL OF FIREBALL
+----------------------------
+item_4 = {
+	name = "Scroll of Fireball",
+	description = "This ragged old scroll\ncan burn everything\nto fine crisp!",
+	max_number = 25,
+	weight = 0.2,
+	value = 25,
+	category = "BOOKS",
+	equipable = false,
+	craftable = true,
+	iconID = 3
+}
+
+----------------------------
+-- RAW SALMON
+----------------------------
+item_5 = {
+	name = "Raw Salmon",
+	description = "The fish smells like\nthe river. I could\neat it raw!",
+	max_number = 1,
+	weight = 1.5,
+	value = 5,
+	category = "FOOD",
+	equipable = false,
+	craftable = false,
+	iconID = 4
+}
