4ad8dd5a0e620252425f74e06d9b5d347e907ae5
Gensthaler Octavian <octav.gensthaler@gmail.com>
2014-07-16 12:40:49 -0700

    Updated item icons and added new items

--- Content/Scripts/items.lua
+++ Content/Scripts/items.lua
@@ -11,13 +11,33 @@ item_0 = {
 	equipable = "False",
 	craftable = "True",
 	consumable = "True",
+	can_sell = "True",
+	can_drop = "True",
+	iconID = "0"
+}
+
+----------------------------
+-- POTION OF MANA
+----------------------------
+item_1 = {
+	name = "Potion of Mana",
+	description = "A strange blue liquid lies at the bottom of the bottle. It's most certainly a mana potion, meant to help me focus on my magic abilities.",
+	max_number = "5",
+	weight = "0.5",
+	value = "25",
+	category = "FOOD",
+	equipable = "False",
+	craftable = "True",
+	consumable = "True",
+	can_sell = "True",
+	can_drop = "True",
 	iconID = "0"
 }
 
 ----------------------------
 -- HEALTH FLASK
 ----------------------------
-item_1 = {
+item_2 = {
 	name = "Health Flask",
 	description = "This container holds more than a bottle; advanced alchemists have designed this sturdy bottle which packs light and allows more sips to be taken before it's depleted.",
 	max_number = "1",
@@ -27,13 +47,33 @@ item_1 = {
 	equipable = "False",
 	craftable = "True",
 	consumable = "True",
+	can_sell = "True",
+	can_drop = "True",
+	iconID = "1"
+}
+
+----------------------------
+-- MANA FLASK
+----------------------------
+item_3 = {
+	name = "Mana Flask",
+	description = "This container holds more than a bottle; advanced alchemists have designed this sturdy bottle which packs light and allows more sips to be taken before it's depleted.",
+	max_number = "1",
+	weight = "1",
+	value = "50",
+	category = "FOOD",
+	equipable = "False",
+	craftable = "True",
+	consumable = "True",
+	can_sell = "True",
+	can_drop = "True",
 	iconID = "1"
 }
 
 ----------------------------
 -- RUSTY BLADE
 ----------------------------
-item_2 = {
+item_4 = {
 	name = "Rusty Blade",
 	description = "Slicing, but not so much dicing...",
 	max_number = "1",
@@ -43,13 +83,15 @@ item_2 = {
 	equipable = "True",
 	craftable = "True",
 	consumable = "False",
+	can_sell = "True",
+	can_drop = "True",
 	iconID = "7"
 }
 
 ----------------------------
 -- DUSTY TOME
 ----------------------------
-item_3 = {
+item_5 = {
 	name = "Dusty Tome",
 	description = "The letters on the title are all scratched. I can't read them...",
 	max_number = "1",
@@ -59,13 +101,15 @@ item_3 = {
 	equipable = "False",
 	craftable = "False",
 	consumable = "False",
+	can_sell = "False"
+	can_drop = "True"
 	iconID = "2"
 }
 
 ----------------------------
 -- SCROLL OF FIREBALL
 ----------------------------
-item_4 = {
+item_6 = {
 	name = "Scroll of Fireball",
 	description = "This ragged old scroll has some words written on it. If I chant these words and make the proper gesture, I'll be able to cast a fiery spell that will burn everything to a fine crisp!",
 	max_number = "25",
@@ -75,13 +119,15 @@ item_4 = {
 	equipable = "False",
 	craftable = "True",
 	consumable = "False",
+	can_sell = "True",
+	can_drop = "True",
 	iconID = "3"
 }
 
 ----------------------------
 -- RAW SALMON
 ----------------------------
-item_5 = {
+item_7 = {
 	name = "Raw Salmon",
 	description = "It's not cooked, and it's starting to smell pretty bad as the scales are rubbing against my bag. I'd better cook it soon or it will go stale.",
 	max_number = "1",
@@ -91,5 +137,7 @@ item_5 = {
 	equipable = "False",
 	craftable = "False",
 	consumable = "True",
+	can_sell = "True",
+	can_drop = "True",
 	iconID = "4"
 }
